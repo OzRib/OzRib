a5d56f35c6f56814c7b6efbc1bdc3eccb6f03686
OzRib <ozeiasnascimentoribeiro@gmail.com>
2022-05-19 16:13:58 -0300

    Fixing css errors in production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
 import '@styles/globals.css'
-import { ThemeProvider } from '@mui/material'
+import { ThemeProvider, StyledEngineProvider, CssBaseline } from '@mui/material'
 import theme from '@themes/root'
 import Header from '@components/globals/Header'
 import type { CustomAppProps } from '@type/index'
 
 function MyApp({ Component, pageProps }: CustomAppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Header
-        headerHeight={Component.headerHeight}
-      />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header
+          headerHeight={Component.headerHeight}
+        />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StyledEngineProvider>
   )
 }
 
